e21396fd52462fc73b324e83b308688ff3d2c784
maurotlxd <maurocanaleszapata@gmail.com>
2019-03-16 04:30:22 +0100

    ejemplo6_validate

---
+++ ejemplo6/js/validar.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+jQuery.validator.addMethod("claveValidate", function(value, element) {
+  return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(value);
+}, '<span style="color:red;">Escribe una contraseña correcta.</span>');
+
+jQuery.validator.addMethod("stringValidate", function(value, element) {
+  return this.optional(element) || /^[a-zA-Z]+$/.test(value);
+}, '<span style="color:red;">Ingrese solo letras.</span>');
+
+jQuery.validator.addMethod("userValidate", function(value, element) {
+  return this.optional(element) || /^[a-zA-Z0-9]+$/.test(value);
+}, '<span style="color:red;">Ingrese letras o números.</span>');
+ /*
+Los datos del  formulario serán enviados solo cuando se hayan cumplido
+las reglas de validacion.
+*/
+$("#frmEjemplo6").validate({
+	rules:{
+		nombres: "stringValidate",
+		apellidos: "stringValidate",
+		usuario : "userValidate",
+		clave: "claveValidate",
+		claveConfirm:{
+			equalTo: "#clave"
+		}
+	},
+           highlight: function (element) {
+           	   $(element).removeClass('is-valid');
+               $(element).addClass('is-invalid');
+           },
+           unhighlight: function (element) {
+               $(element).removeClass('is-invalid');
+               $(element).addClass('is-valid');
+           }
+});
+});
